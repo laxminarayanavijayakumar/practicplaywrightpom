238268e6d4d4ca26974b18e53219b9b9088817a4
balaji-vijayakumar-dotcom <laxminarayanavijayakumar@gmail.com>
2025-06-05 16:46:06 +0530

    gitactions

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
-import allurePlaywright from 'allure-playwright';
 
 /**
  * Read environment variables from file.
@@ -14,9 +13,9 @@ import allurePlaywright from 'allure-playwright';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -24,30 +23,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],['allure-playwright']],
-  // ,['allure-playwright',{outputFolder:'my-allure-results'}]
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    video:'retain-on-failure',
-    screenshot:'only-on-failure'
-    
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'Google Chrome',
-      use: {
-        channel: 'chrome',
-        screenshot:"only-on-failure",
-        video:"retain-on-failure",
-      },
-    },
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
@@ -87,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
+  //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
 });
