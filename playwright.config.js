79f6f6da6dcf629ac2bf483d8930fe7072253e29
balaji-vijayakumar-dotcom <laxminarayanavijayakumar@gmail.com>
2025-06-05 17:22:14 +0530

    minorfix

--- playwright.config.js
+++ playwright.config.js
@@ -34,6 +34,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot:'only-on-failure',
     video:'retain-on-failure',
+    headless:false
   },
 
   /* Configure projects for major browsers */
@@ -41,25 +42,26 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
-        trace:"retain-on-failure"
+        // trace:"retain-on-failure"
        },
     },
-    {
-       name: 'Google Chrome',
-       use: { 
-        channel: 'chrome',
-       },
-     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //    name: 'Google Chrome',
+    //    use: { 
+    //     channel: 'chrome',
+    //    },
+    //  },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
